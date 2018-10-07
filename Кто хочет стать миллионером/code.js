704d6ae5afd92dcf693dc3356a57681e8c050a81
maslenk <maslenk@mail.ru>
2018-10-07 15:56:45 +0700

    Ошибка
    Исправил ошибку, не менялся массив с ответами

--- Кто хочет стать миллионером/code.js
+++ Кто хочет стать миллионером/code.js
@@ -1,7 +1,8 @@
 var a, i;
 var c=0;
 for (i=0; i<question.length; i++) {
-	a = +prompt(question[i] + "\n" + answer1[0] + "\n" + answer1[1] + "\n" + answer1[2] + "\n" + answer1[3] + "\n" + "(Правильный ответ "+ correctAnswer[i] + ")");
+	answerI(i);
+	a = +prompt(question[i] + "\n" + answer[0] + "\n" + answer[1] + "\n" + answer[2] + "\n" + answer[3] + "\n" + "(Правильный ответ "+ correctAnswer[i] + ")");
 	if (a != correctAnswer[i]) {
 		alert ("Вы проиграли!!!");
 		break;
@@ -12,3 +13,23 @@ for (i=0; i<question.length; i++) {
 	}
 }
 alert("Вы ответили правильно на " + c + " из " + question.length);
+
+function answerI() {
+	switch(i) {
+		case 0: 
+			answer = answer1;
+			break;
+		case 1:
+			answer = answer2;
+			break;
+		case 2:
+			answer = answer3;
+			break;
+		case 3:
+			answer = answer4;
+			break;
+		case 4:
+			answer = answer5;
+			break;
+	}
+}
